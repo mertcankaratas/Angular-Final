b9cfe1b9328a2b7cc3fa5c52dda1ffd29c703d80
Mertcan Karataş <can.mert.karatas@gmail.com>
2021-10-23 17:24:04 +0300

    component ve service eklendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,8 @@
+import { ProductService } from './../../services/product.service';
+import { ProductResponseModel } from './../../models/productResponseModel';
+import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-product',
@@ -7,17 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product1 = { productId: 1, productName: 'Bardak', categoryId: 2, unitPrice: 5 };
-  product2 = { productId: 2, productName: 'Laptop', categoryId: 2, unitPrice: 5 };
-  product3 = { productId: 3, productName: 'Mouse', categoryId: 2, unitPrice: 5 };
-  product4 = { productId: 4, productName: 'Keyboard', categoryId: 2, unitPrice: 5 };
-  product5 = { productId: 5, productName: 'Camera', categoryId: 2, unitPrice: 5 };
 
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
 
-  constructor() { }
+  products: Product[] = [];
+  dataLoaded=false;
+
+
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+
+    this.productService.getProducts().subscribe(response =>{
+      this.products=response.data
+      this.dataLoaded=true;
+    });
+
+
   }
 
 }
